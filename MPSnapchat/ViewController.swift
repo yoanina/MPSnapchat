f8ad8fe4a23e0bf39c6f3c9eb7d6129b796fb14d
yoanina mamani <yoanina.mamani@tecsup.edu.pe>
2023-06-07 09:43:45 -0500

    Initial Commit

---
+++ MPSnapchat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MPSnapchat
+//
+//  Created by Yoanina Mamani Paredes on 7/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
